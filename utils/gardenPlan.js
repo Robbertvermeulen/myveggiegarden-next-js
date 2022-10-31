ac2af6429dbbf2cf26c95003acbc523b413af101
Robbert <robbert@Tests-MacBook-Air.local>
2022-10-31 11:29:04 +0100

    Remove garden plan context

--- utils/gardenPlan.js
+++ utils/gardenPlan.js
@@ -1,19 +1,23 @@
 import { isInTimeframe } from "./time";
 
-export const weekHasContents = (gardenPlan, week, year) => {
+export const weekHasContents = ({ areas, seedlings }, week, year) => {
   let result = { plants: null, seedlings: null };
 
-  gardenPlan?.areas.forEach((area) =>
-    area.soil.forEach((soilObj) =>
-      soilObj.contents.forEach((content) => {
-        if (isInTimeframe({week, year}, content.timeframe))
-          result.plants = true;
-      })
-    )
-  );
-  gardenPlan?.seedling.forEach((seedling) => {
-    if (isInTimeframe({week, year}, seedling.timeframe))
-      result.seedlings = true;
-  });
+  if (areas)
+    areas.forEach((area) =>
+      area.soil.forEach((soilObj) =>
+        soilObj.contents.forEach((content) => {
+          if (isInTimeframe({ week, year }, content.timeframe))
+            result.plants = true;
+        })
+      )
+    );
+
+  if (seedlings)
+    seedlings.forEach((seedling) => {
+      if (isInTimeframe({ week, year }, seedling.timeframe))
+        result.seedlings = true;
+    });
+
   return result;
 };
